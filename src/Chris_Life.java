4037938a6dd9b3fefc50e6c2bd918fce9bbcd852
Zeeker <ezekiel.ramirez@calbaptist.edu>
2022-09-17 09:09:54 -0700

    Added notes

--- src/Chris_Life.java
+++ src/Chris_Life.java
@@ -12,8 +12,8 @@ import java.util.Random;
 //        import java.util.Vector;
 
 public class Chris_Life {
-    final static int maxGridHeight = 3;
-    final static int maxGridLength = 3;
+    final static int maxGridHeight = 5;
+    final static int maxGridLength = 5;
     static int[][] grid1 = new int[maxGridLength][maxGridHeight]; // the init grid
     static int[][] grid2 = new int[maxGridLength][maxGridHeight]; // the holder grid
     static int numSteps = 2;
@@ -34,9 +34,41 @@ public class Chris_Life {
         grid1[2][2] = 1;
         grid1[2][3] = 1;*/
 
-        for (int w = 0; w < numSteps; w++) { // do this life simulation for 10 steps
-            for (int col = 0; col < maxGridLength; col++) {
-                for (int row = 0; row < maxGridHeight; row++) {
+        // why is this col then row, I will try row then col
+//        for (int w = 0; w < numSteps; w++) { // do this life simulation for 10 steps
+//            for (int col = 0; col < maxGridLength; col++) {
+//                for (int row = 0; row < maxGridHeight; row++) {
+//                    int numFriends = getNumFriends(col, row, grid1);
+//                    System.out.println("Location: " + "(" + row + "," + col + ") " + "Friends: " + numFriends);
+//                    if (grid1[row][col] == 1 && numFriends < 2) {
+//                        grid2[row][col] = 0;
+//                    } else if (grid1[row][col] == 1 && numFriends < 4) {
+//                        grid2[row][col] = 1;
+//                    } else if (grid1[row][col] == 1 && numFriends > 3) {
+//                        grid2[row][col] = 0;
+//                    } else if (grid1[row][col] == 0 && numFriends == 3) {
+//                        grid2[row][col] = 1;
+//                    } else {
+//                        //grid2[row][col] = 0;
+//                    }
+//
+//                }
+//            }
+//            grid1 = grid2;
+//            printGrid(grid2);
+//            System.out.println();
+//        }
+
+        // Here is my solution
+        for (int w = 0; w < numSteps; w++) {
+            for (int row = 0; row < maxGridHeight; row++) {
+                for (int col = 0; col < maxGridLength; col++) {
+                    // the rules
+                    //1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+                    //2. Any live cell with two or three live neighbours lives on to the next generation.
+                    //3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+                    //4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction
+
                     int numFriends = getNumFriends(col, row, grid1);
                     System.out.println("Location: " + "(" + row + "," + col + ") " + "Friends: " + numFriends);
                     if (grid1[row][col] == 1 && numFriends < 2) {
@@ -81,13 +113,18 @@ public class Chris_Life {
     // TODO do checks on values away from edges, then use if stmts to make edge cases
     // we are working with arrays, refer to that syntax
     // int[row][col]
-    public static int getNumFriends(int col, int row, int[][] a) {
-        return 0;
+    public static int getNumFriends(int col, int row, int[][] a) { // trying to implement wrap around
+        int numF = 0; // glitched somewhere so that phantom friends are around
+        System.out.println("Central Coord: row-" + row + ", col-" + col + "\nValue: " + a[row][col]);
+        // check values by row
+
+        return numF;
     }
+
 
 //    public static int getNumFriends(int col, int row, int[][] a) { // trying to implement wrap around
 //        int numF = 0; // glitched somewhere so that phantom friends are around
-//        System.out.println("Central Coord: " + col + "," + row + "\nValue: " + a[col][row]);
+//        System.out.println("Central Coord: row-" + row + ", col-" + col + "\nValue: " + a[row][col]);
 //        // check values by row
 //        for (int i = 0; i < 3; i++) {
 //            int fx = col - (1 - i);
@@ -112,7 +149,7 @@ public class Chris_Life {
 //                if (a[fx][fy] == 1) {
 //                    numF++;
 //                }
-//                System.out.println("\t checking coords: " + fx + "," + fy + " Friend: " + a[fx][fy]);
+//                System.out.println("\t checking coords: row-" + fx + ", col-" + fy + " Friend: " + a[fx][fy]);
 //            }
 //
 //        }
