7d1f6e51c59eac1e8032634b3e2b7f0fc3c3d546
Zeeker <ezekiel.ramirez@calbaptist.edu>
2022-09-18 13:40:25 -0700

    I am going to restart my version

--- src/Chris_Life.java
+++ src/Chris_Life.java
@@ -12,17 +12,17 @@ import java.util.Random;
 //        import java.util.Vector;
 
 public class Chris_Life {
-    final static int maxGridHeight = 5;
-    final static int maxGridLength = 5;
-    static int[][] grid1 = new int[maxGridLength][maxGridHeight]; // the init grid
-    static int[][] grid2 = new int[maxGridLength][maxGridHeight]; // the holder grid
+    final static int Tot_Rows = 5;
+    final static int Tot_Cols = 5;
+    static int[][] grid1 = new int[Tot_Rows][Tot_Cols]; // the init grid
+    static int[][] grid2 = new int[Tot_Rows][Tot_Cols]; // the holder grid
     static int numSteps = 2;
 
     public static void main(String[] args) {
         Random rand = new Random(); //init the array with random bytes
         // grid[row][col]
-        for (int i = 0; i < maxGridLength; i++) {
-            for (int j = 0; j < maxGridHeight; j++) {
+        for (int i = 0; i < Tot_Rows; i++) {
+            for (int j = 0; j < Tot_Cols; j++) {
                 grid1[i][j] = Math.abs(rand.nextInt() % 2);
                 // grid[r][c]
                 /*grid1[i][j] = 0;*/
@@ -60,41 +60,50 @@ public class Chris_Life {
 //        }
 
         // Here is my solution
-        for (int w = 0; w < numSteps; w++) {
-            for (int row = 0; row < maxGridHeight; row++) {
-                for (int col = 0; col < maxGridLength; col++) {
-                    // the rules
-                    //1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-                    //2. Any live cell with two or three live neighbours lives on to the next generation.
-                    //3. Any live cell with more than three live neighbours dies, as if by overpopulation.
-                    //4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction
+        for (int row = 0; row < Tot_Rows; row++) {
+            for (int col = 0; col < Tot_Cols; col++) {
+                // the rules
+                //1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+                // n < 2
+                //2. Any live cell with two or three live neighbours lives on to the next generation.
+                // n == 2 || n == 3
+                //3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+                // n > 3
+                //4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction
+                // n == 3
 
-                    int numFriends = getNumFriends(col, row, grid1);
-                    System.out.println("Location: " + "(" + row + "," + col + ") " + "Friends: " + numFriends);
-                    if (grid1[row][col] == 1 && numFriends < 2) {
-                        grid2[row][col] = 0;
-                    } else if (grid1[row][col] == 1 && numFriends < 4) {
-                        grid2[row][col] = 1;
-                    } else if (grid1[row][col] == 1 && numFriends > 3) {
-                        grid2[row][col] = 0;
-                    } else if (grid1[row][col] == 0 && numFriends == 3) {
-                        grid2[row][col] = 1;
-                    } else {
-                        //grid2[row][col] = 0;
-                    }
-
+                // 1 = live cell
+                // 0 = unpopulated cell
+                int numFriends = getNumFriends(col, row, grid1);
+                System.out.println("Location: " + "(" + row + "," + col + ") " + "Friends: " + numFriends);
+                // if cell alive and less than 2 n, dies
+                if (grid1[row][col] == 1 && numFriends < 2) {
+                    grid2[row][col] = 0;
+                } else if (grid1[row][col] == 1 && numFriends < 4) {
+                    // if cell alive and less than 4 n, cell lives
+                    grid2[row][col] = 1;
+                } else if (grid1[row][col] == 1 && numFriends > 3) {
+                    // if cell alive and n greater than 3, cell dies
+                    grid2[row][col] = 0;
+                } else if (grid1[row][col] == 0 && numFriends == 3) {
+                    // if cell dead and friends equal 3, new cell born
+                    grid2[row][col] = 1;
+                } else {
+                    //grid2[row][col] = 0;
                 }
             }
+            // make the original the same as the updated grid
             grid1 = grid2;
+            // we could probably print the updated grid1
             printGrid(grid2);
             System.out.println();
         }
     }
 
     public static void printGrid(int[][] b) {
-        for (int i = 0; i < maxGridLength; i++) {
+        for (int i = 0; i < Tot_Cols; i++) {
             printTop();
-            for (int j = 0; j < maxGridHeight; j++) {
+            for (int j = 0; j < Tot_Rows; j++) {
                 System.out.print(grid1[i][j] + "|");
 
             }
@@ -103,7 +112,7 @@ public class Chris_Life {
     }
 
     public static void printTop() {
-        for (int i = 0; i < (maxGridLength * 2) + 1; i++) {
+        for (int i = 0; i < (Tot_Cols * 2) + 1; i++) {
             System.out.print("-");
         }
         System.out.println();
@@ -115,10 +124,22 @@ public class Chris_Life {
     // int[row][col]
     public static int getNumFriends(int col, int row, int[][] a) { // trying to implement wrap around
         int numF = 0; // glitched somewhere so that phantom friends are around
-        System.out.println("Central Coord: row-" + row + ", col-" + col + "\nValue: " + a[row][col]);
+//        System.out.println("Central Coord: row-" + row + ", col-" + col + "\nValue: " + a[row][col]);
         // check values by row
+        // -1 will check left side, 1 is the right side
+        for (int i = -1; i < 1; i++) {
+            // then check each col in a row
+            for (int j = -1; j < 1; j++){
+                // -1 is the upper side, 1 is the lower side
+                // increment numF if friend is in the spot
+                // this if stmt is checking that we are only updating values inside the boundaries
+                if ((i + row >= 0 && i + row < row) && (col + j >= 0 && col + j < col)) {
+                    numF += a[i + row][col + j];
+                }
+            }
+        }
 
-        return numF;
+        return numF + 0;
     }
 
 
